d87a27f4d90e79d8d026cbcc729c738ffa4f681d
LucasNova <PogorelowMax@gmail.com>
2015-04-06 01:42:18 +0600

    changes

--- www/app/controller/portfolio/portfolioController.js
+++ www/app/controller/portfolio/portfolioController.js
@@ -1,6 +1,6 @@
 (function() {
-    'use strict';
-    app.controller('portfolioController', function($scope, $routeParams) {
+
+    app.controller('portfolioController', function($scope, $routeParams, $http) {
         var $appScope = getScope('appCtrl');
         $appScope.template.closeMenu();
         $appScope.app.pageClass = 'page-portfolio';
@@ -10,6 +10,44 @@
 
         this.name = "portfolioController";
         this.params = $routeParams;
+
+        this.categories = [];
+        this.previews = [];
+
+        this.currentPreview = {};
+        this.previousPreview = {};
+        this.nextPreview = {};
+
+        this.server = {
+            getCategories: function(){
+                $http.get('db/categories.json').success(function(data) {
+                    $scope.portfolio.categories = data;
+                });
+            },
+            getPreviews: function(){
+                $http.get('db/gallery.json').success(function(data) {
+                    $scope.portfolio.previews = data;
+                    $scope.portfolio.currentPreview = _.findWhere($scope.portfolio.previews, { pageUrl: $scope.portfolio.pageName });
+
+                    if ($scope.portfolio.previews.indexOf($scope.portfolio.currentPreview) == 0){
+                        $scope.portfolio.previousPreview = $scope.portfolio.previews[$scope.portfolio.previews.length - 1];
+                    }else{
+                        $scope.portfolio.previousPreview = $scope.portfolio.previews[$scope.portfolio.previews.indexOf($scope.portfolio.currentPreview) - 1];
+                    }
+
+                    if ($scope.portfolio.previews.indexOf($scope.portfolio.currentPreview) == $scope.portfolio.previews.length - 1){
+                        $scope.portfolio.nextPreview = $scope.portfolio.previews[0];
+                    }else{
+                        $scope.portfolio.nextPreview = $scope.portfolio.previews[$scope.portfolio.previews.indexOf($scope.portfolio.currentPreview) + 1];
+                    }
+
+                });
+            }
+        };
+
+        this.server.getCategories();
+        this.server.getPreviews();
+
 
         if ($routeParams.page)
             this.pageName = $routeParams.page;
