fb667330e66ef2e62b3de66e133498cb1e3a8b75
LucasNova <pogorelowMax@gmail.com>
2015-06-11 01:32:28 +0600

    delete www

--- www/public/edge/spheres_edgePreload.js
+++
@@ -1,4 +0,0 @@
-
-(function(comp){"use strict";var preloaderDOM={dom:[{rect:['540','300','120px','120px','auto','auto'],id:'preloader4',fill:['rgba(0,0,0,0)','images/preloader4.gif','0px','0px'],type:'image',tag:'img'}],style:{'${symbolSelector}':{isStage:'true',rect:[undefined,undefined,'1200px','720px']}}}
-comp.definePreloader(preloaderDOM);}(AdobeEdge.getComposition("EDGE-1636752")));(function(comp){"use strict";var downLevelStageDOM={dom:[],style:{}}
-comp.defineDownLevelStage(downLevelStageDOM);}(AdobeEdge.getComposition("EDGE-1636752")));
